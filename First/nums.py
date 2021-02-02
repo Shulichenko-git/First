a2878af5c469046c1bd962f4ecc400f61a634ee9
Artem Shulichenko <shulichenko.a@gmail.com>
2021-02-02 23:40:54 +0200

    add func mul add module strings.py with func up

--- First/nums.py
+++ First/nums.py
@@ -5,7 +5,10 @@ def summa(a, b):
 def minus(a, b):
     return a - b
 
+def mul(a, b):
+    return a * b
 
 if __name__ == '__main__':
     print(summa(3, 4))
     print(minus(3, 1))
+    print(mul(2, 4))
